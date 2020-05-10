1430867fca0b8e0fed6c5ef2cee14e96fb8a3c90
YuYue <yuyue.self@bytedance.com>
2020-05-10 15:46:39 +0800

    Fix Package.swift issue

--- Package.swift
+++ Package.swift
@@ -1,7 +1,20 @@
+// swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(
     name: "YYSwift",
-    dependencies : [],
-    exclude: ["Tests"]
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v9),
+        .watchOS(.v2),
+        .macOS(.v10_14)
+    ],
+    products: [
+        .library(name: "YYSwift", targets: ["YYSwift"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "YYSwift", dependencies: [], path: "Sources")
+    ]
 )
